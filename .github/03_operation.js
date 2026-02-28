2d1c8af3844d5abe6e7e999e5207973a534521d1
Prakhar Raj <rajprakhar2505@gmail.com>
2026-02-28 12:16:09 +0000

    updated js files

---
+++ .github/03_operation.js
@@ -0,0 +1,23 @@
+let score="55"
+//console.log(typeof score)
+
+let value=Number(score)
+//console.log(typeof value)
+
+
+//"55" --33
+//"55abc"--NaN
+//TRUE---1,FALSE--0
+
+let boolvalue="KARTIK"
+
+let valuebool=Boolean(boolvalue)
+//console.log(valuebool)
+
+//1--true, 0--false
+//empty--0, 
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+//===is used to check datatype according to that give boolean o/p
---
+++ 01_basics/summary.js
@@ -0,0 +1,31 @@
+//categorized on th basis of access and storage
+
+
+//primitive
+// 7 types :string,boolean,Num,Null,Undefined,Symbol,BigInt
+
+//Reference (non Primitive)
+
+//Array,Objects,Functions 
+const hero=["a","b","c"]; 
+const func=Function()
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+
+
+*/
